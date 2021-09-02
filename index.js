8928a9ec3cdb3f354c858286c468e746f346fd71
Saim Rizvi <shr2mz@gmail.com>
2021-09-01 23:31:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector('#main')
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'Saim is the champion'
